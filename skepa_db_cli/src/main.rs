ec36ce208d0dd1d5d3f44abbc5e9359ac9e28e6f
Aayush <94561475+AayushMainali-Github@users.noreply.github.com>
2026-01-26 13:53:39 +0530

    feat: add a basic repl

--- skepa_db_cli/src/main.rs
+++ skepa_db_cli/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use skepa_db_core::Database;
+use std::io::{self, Write};
+
+fn main(){
+    let mut db = Database::open("./mydb");
+
+    println!("skepa_db_cli (type 'help' or 'exit)");
+
+    loop {
+        print!("db> ");
+        io::stdout().flush().unwrap();
+
+        let mut line = String::new();
+        if io::stdin().read_line(&mut line).is_err(){
+            println!("Failed to read input");
+            continue;
+        }
+
+        let input = line.trim();
+        if input.is_empty() {
+            continue;
+        }
+
+        if input.eq_ignore_ascii_case("exit") || input.eq_ignore_ascii_case("quit"){
+            break;
+        }
+
+        if input.eq_ignore_ascii_case("help"){
+            println!("Commands: anything -> ok, exit, help");
+            continue;
+        }
+
+        match db.execute(input) {
+            Ok(out) => println!("{out}"),
+            Err(err) => println!("{err}"),
+        }
+    }
 }
