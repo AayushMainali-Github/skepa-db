1e7fdc9f7817cb16f8254ee2be05df2989513453
Aayush <94561475+AayushMainali-Github@users.noreply.github.com>
2026-01-26 13:43:52 +0530

    feat: add a basic database struct

--- skepa_db_core/src/lib.rs
+++ skepa_db_core/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Database {
+    path: PathBuf,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Database {
+    pub fn open(path: impl Into<PathBuf>) -> Self {
+        Self { path: path.into() }
     }
+
+    pub fn execute(&mut self, _input: &str) -> Result<String,String>{
+        Ok("Ok".to_string())
+    }
+
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    
 }
